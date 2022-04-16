39434d2e7d0a335b56454dfbb9dffe61b61c7aec
NathanWelsh <95314170+NathanWelsh@users.noreply.github.com>
2022-04-16 00:14:26 -0400

    Add files via upload

---
+++ cartSIMGiveToStudents/tc_order.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+  
+   Professor: Dr. Roger Webster
+   Code: Nathan Welsh 
+     
+   Filename: tc_order.js  
+
+   The item array contains the ID numbers of the items ordered by the customer
+   The itemDescription array contains the description of each item
+   The itemPrice array contains the price of each item
+   The itemQty array contains the quantity ordered of each item
+   
+*/
+
+var item = ["10582", "23015", "41807", "10041"];
+var itemDescription = ["1975 Green Bay Packers Football (signed), Item 10582",
+                        "Tom Landry 1955 Football Card (unsigned), Item 23015",
+                        "1916 Army-Navy Game, Framed Photo (signed), Item 41807",
+                        "Protective Card Sheets, Item 10041"];
+var itemPrice = [28149.93, 4589.98, 1334.93, 22.67];
+var itemQty = [1, 1, 1, 4];
+
+function formatDollar(num) {
+   var p = num.toFixed(2).split(".");
+   return "$" + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
+   return num + (num != "-" && i && !(i % 3) ? "," : "") + acc;
+   }, "") + "." + p[1];
+   }
